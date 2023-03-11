13ccd60a918df8565043b31a5e53cf5d0dcc6e10
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-03-12 01:47:20 +0600

    updated footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { FaFacebookSquare, FaGithubSquare, FaTwitterSquare } from 'react-icons/fa';
 
 const Footer = () => {
 
     const date = new Date().getFullYear();
 
     return (
-        <footer>
-            <p>Copyright &copy; {date}, All Rights Reserved</p>
+        <footer className='bg-dark d-flex justify-content-evenly align-items-center flex-column'>
+            <div className='text-white mb-2 fs-1'>
+                <FaFacebookSquare  className='me-2'></FaFacebookSquare>
+                <FaTwitterSquare className='me-2'></FaTwitterSquare >
+                <FaGithubSquare ></FaGithubSquare>
+            </div>
+            <div className='d-flex'>
+                <p className='text-white'>Copyright &copy; {date}, All Rights Reserved</p>
+            </div>
         </footer>
     );
 };
