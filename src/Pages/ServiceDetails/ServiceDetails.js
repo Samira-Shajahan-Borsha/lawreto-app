4209e236927556517c209a81f7efc2d9fcca0590
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-03-12 01:08:01 +0600

    firebase config changed

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
-import { FaExclamationCircle } from 'react-icons/fa';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { FaExclamationCircle, FaUser } from 'react-icons/fa';
 import { useLoaderData } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
 
@@ -19,25 +19,22 @@ const ServiceDetails = () => {
         mobileNumberError: ''
     });
 
+    const [submitted, setSubmitted] = useState(false);
+
 
     console.log(user);
 
     const service = useLoaderData();
 
     console.log(service);
+
     const { name } = service;
 
     const handleSubmit = event => {
         event.preventDefault();
-
         const form = event.target;
-        const name = form.name.value;
-
-        const address = form.address.value;
-
         form.reset();
-
-        console.log(name, address);
+        setSubmitted(true);
     }
 
     const handleNameBlur = nameInput => {
@@ -63,7 +60,7 @@ const ServiceDetails = () => {
     }
 
     const handleMobileNumberBlur = mobileNumberInput => {
-        
+
         const numberRegex = /^01[3-9][0-9]{8}$/;
 
         const validMobileNumber = numberRegex.test(mobileNumberInput);
@@ -85,39 +82,59 @@ const ServiceDetails = () => {
 
     return (
         <Container className='mx-lg-auto my-5 px-lg-5'>
-            <h1 className='fs-3 text-success mb-3 text-center'>{name}</h1>
-            <Form onSubmit={handleSubmit} className='mx-lg-5 px-lg-5'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label className='fs-6 fw-semibold'>User Name</Form.Label>
-                    <Form.Control onBlur={(event) => handleNameBlur(event.target.value)} type="text" name='name' placeholder="Enter your name" className='border-dark rounded-0' required />
-                    {
-                        errors?.nameError
-                        &&
-                        <p className='text-danger'><FaExclamationCircle className='me-1'></FaExclamationCircle>{errors?.nameError}</p>
-                    }
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label className='fs-6 fw-semibold'>Email address</Form.Label>
-                    <Form.Control type="email" name='email' readOnly defaultValue={user?.email} className='border-dark rounded-0' required />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label className='fs-6 fw-semibold'>Address</Form.Label>
-                    <Form.Control onBlur={(event) => handleAddressBlur(event.target.value)} name='address' as="textarea" placeholder='Enter your address' className='border-dark rounded-0' required rows={3} />
-                    {
-                        errors?.addressError && <p className='text-danger'><FaExclamationCircle className='me-1'></FaExclamationCircle>{errors?.addressError}</p>
-                    }
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label className='fs-6 fw-semibold'>Mobile Number</Form.Label> <br />
-                    <input type="tel" onBlur={(event) => handleMobileNumberBlur(event.target.value)} name="mmobileNumber" pattern="^01[3-9][0-9]{8}$" id="" className='w-50 form-control border-dark rounded-0' required />
-                    {
-                        errors?.mobileNumberError && <p className='text-danger'><FaExclamationCircle className='me-1'></FaExclamationCircle>{errors?.mobileNumberError}</p>
-                    }
-                </Form.Group>
-                <Button variant="success" className='fw-semibold  rounded-pill text-dark px-lg-5 py-lg-2 px-4 py-2' type="submit">
-                    BOOK YOUR SERVICE NOW
-                </Button>
-            </Form>
+            <h1 className='fs-1 text-success mb-3 text-center fw-bold mb-5'>{name}</h1>
+            {
+                submitted
+                    ?
+                    <Row className='align-items-center p-5'>
+                        <Col className='w-100 d-flex justify-content-center py-5'>
+                            {
+                                user?.photoURL ?
+                                    <img src={user?.photoURL} alt="imageURL"/>
+                                    :
+                                    <FaUser className='w-50'></FaUser>
+                            }
+                        </Col>
+                        <Col>
+                            <p className='fs-1 text-dark fw-semibold'>Thank you for Booking! {user?.displayName}</p>
+                        </Col>
+                    </Row>
+                    :
+                    <Form onSubmit={handleSubmit} className='mx-lg-5 px-lg-5'>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className='fs-6 fw-semibold'>User Name</Form.Label>
+                            <Form.Control onBlur={(event) => handleNameBlur(event.target.value)} type="text" name='name' placeholder="Enter your name" className='border-dark rounded-0' required />
+                            {
+                                errors?.nameError
+                                &&
+                                <p className='text-danger'><FaExclamationCircle className='me-1'></FaExclamationCircle>{errors?.nameError}</p>
+                            }
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className='fs-6 fw-semibold'>Email address</Form.Label>
+                            <Form.Control type="email" name='email' readOnly defaultValue={user?.email} className='border-dark rounded-0' required />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label className='fs-6 fw-semibold'>Address</Form.Label>
+                            <Form.Control onBlur={(event) => handleAddressBlur(event.target.value)} name='address' as="textarea" placeholder='Enter your address' className='border-dark rounded-0' required rows={3} />
+                            {
+                                errors?.addressError && <p className='text-danger'><FaExclamationCircle className='me-1'></FaExclamationCircle>{errors?.addressError}</p>
+                            }
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label className='fs-6 fw-semibold'>Mobile Number</Form.Label> <br />
+                            <input type="tel" onBlur={(event) => handleMobileNumberBlur(event.target.value)} name="mmobileNumber" pattern="^01[3-9][0-9]{8}$" id="" className='w-50 form-control border-dark rounded-0' required />
+                            {
+                                errors?.mobileNumberError && <p className='text-danger'><FaExclamationCircle className='me-1'></FaExclamationCircle>{errors?.mobileNumberError}</p>
+                            }
+                        </Form.Group>
+                        <Button variant="success" className='fw-semibold  rounded-pill text-dark px-lg-5 py-lg-2 px-4 py-2' type="submit">
+                            BOOK YOUR SERVICE NOW
+                        </Button>
+                    </Form>
+
+
+            }
         </Container>
     );
 };
